d71787b5e7bf5214758e29257fb520e4c131a99c
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-17 16:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_1" {
-  ami           = ""
+  ami           = "ami-0cea098ed2ac54925"
   instance_type = "t2.micro"
   subnet_id     = data.aws_subnet.selected.id
   key_name = var.my_key
